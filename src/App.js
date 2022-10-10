f8c59eb7328d3bff2ab97e586afb6ad1aad32072
Vionaerika <vionaerikakwok@gmail.com>
2022-10-10 16:04:03 +0800

    populated results on every element in the array of data

--- src/App.js
+++ src/App.js
@@ -6,6 +6,24 @@ function App() {
     <div>
       <h2>Expense Tracker</h2>
       {/*declare of variable*/}
+      {expenses.map((expense) => {
+        return (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          ></ExpenseItem>
+        );
+      })}
+
+      {/* for list of lists
+      {expenses.map((expense) => {
+        return expense.map((ele) => {
+          return (
+            <ExpenseItem title={ele} amount={ele} date={ele}></ExpenseItem>
+          );
+        });
+      })} */}
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
